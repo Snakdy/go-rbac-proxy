bd02916fbc53eab64e45e0dbfbfc4c612fcfc225
Django Cass <django@dcas.dev>
2022-05-24 21:47:11 +1000

    basic http app foundation

--- cmd/rbac/main.go
+++ cmd/rbac/main.go
@@ -1,1 +1,37 @@
-package rbac
+package main
+
+import (
+	"context"
+	"github.com/djcass44/go-utils/logging"
+	"github.com/gorilla/mux"
+	"github.com/kelseyhightower/envconfig"
+	"gitlab.com/autokubeops/serverless"
+	"go.uber.org/zap"
+	"net/http"
+)
+
+type environment struct {
+	Port int `envconfig:"PORT" default:"8080"`
+}
+
+func main() {
+	// read environment
+	var e environment
+	envconfig.MustProcess("app", &e)
+
+	// configure logging
+	zc := zap.NewProductionConfig()
+	log, _ := logging.NewZap(context.TODO(), zc)
+
+	// configure routing
+	router := mux.NewRouter()
+	router.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("OK"))
+	})
+
+	// start serving
+	serverless.NewBuilder(router).
+		WithLogger(log).
+		WithPort(e.Port).
+		Run()
+}
